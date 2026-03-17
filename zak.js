089f080a135c2e04fcae4c5a1acba7db8757d829
Zakaria Nouhou <zakarianouhou418@gmail.com*>
2026-03-17 15:33:42 +0000

    Premier commit

---
+++ zak.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function() {
+  const bouton = document.getElementById("telecharger");
+
+    bouton.addEventListener("click", function() {
+      alert("CV téléchargé avec succès!");
+    });
+  });
